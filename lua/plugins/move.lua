0b1d2e8233367a56da9c9045eb24cb4f847044fb
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-21 07:38:24 -0500

    feat(move): add move-analyzer LSP

--- lua/plugins/move.lua
+++ lua/plugins/move.lua
@@ -1,4 +1,32 @@
 return {
-  "yanganto/move.vim",
-  branch = "sui-move",
+  {
+    "yanganto/move.vim",
+    branch = "sui-move",
+  },
+  {
+    "AstroNvim/astrolsp",
+    opts = function(_, opts) opts.servers = require("astrocore").list_insert_unique(opts.servers, { "move_analyzer" }) end,
+  },
+  { -- Provide the actual config for nvim-lspconfig
+    -- (AstroLSP calls lspconfig setup automatically once listed in servers)
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        move_analyzer = {
+          -- cmd = { "move-analyzer" },          -- usually not needed if in PATH
+          -- but if you want to be explicit / override:
+          -- cmd = { "/full/path/to/move-analyzer" },
+
+          filetypes = { "move" },
+
+          -- root_dir patterns (important for project detection)
+          -- Most Move projects have Move.toml at the root
+          root_dir = function(fname) return require("lspconfig.util").root_pattern("Move.toml", ".git")(fname) end,
+
+          -- If your move-analyzer supports extra settings (rarely needed)
+          -- settings = { ... },
+        },
+      },
+    },
+  },
 }
