7bbba6ce67989c9e374f6f70c6c90aadeec1f4fb
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-02-24 16:09:50 -0500

    feat(plugins/astrolsp): configure nixd

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -53,6 +53,26 @@ return {
           },
         },
       },
+      nixd = {
+        settings = {
+          nixd = {
+            nixpkgs = {
+              expr = "import <nixpkgs> { }",
+            },
+            formatting = {
+              command = { "alejandra" }, -- or nixfmt or nixpkgs-fmt
+            },
+            -- options = {
+            --   nixos = {
+            --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options',
+            --   },
+            --   home_manager = {
+            --       expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options',
+            --   },
+            -- },
+          },
+        },
+      },
     })
   end,
 }
