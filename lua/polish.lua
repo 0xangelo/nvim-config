1a3be2e1d8fc34f5ac07a6dc442e5a35598fe9d2
Angelo Gregorio Lovatto (GitHub) <12701614+0xangelo@users.noreply.github.com>
2026-03-29 13:25:51 -0500

    fix(polish): disable kitty margin adjustment if running neovide

--- lua/polish.lua
+++ lua/polish.lua
@@ -6,16 +6,17 @@
 
 -- Kitty margin management
 -- https://www.reddit.com/r/KittyTerminal/comments/1auxxcc/comment/nfzpe77/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.cmd "silent !kitty @ set-spacing margin=-10"
-    -- vim.defer_fn(function() vim.cmd "silent !kitty @ set-spacing margin=-10" end, 100)
-  end,
-})
-
-vim.api.nvim_create_autocmd("VimLeave", {
-  callback = function() vim.cmd "silent !kitty @ set-spacing margin=default" end,
-})
+if not vim.g.neovide then
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      vim.cmd "silent !kitty @ set-spacing margin=-10"
+      -- vim.defer_fn(function() vim.cmd "silent !kitty @ set-spacing margin=-10" end, 100)
+    end,
+  })
+  vim.api.nvim_create_autocmd("VimLeave", {
+    callback = function() vim.cmd "silent !kitty @ set-spacing margin=default" end,
+  })
+end
 
 vim.opt.wrap = true          -- enable visual line wrapping
 vim.opt.linebreak = true     -- wrap at word boundaries (don't split words)
